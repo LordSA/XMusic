fd43e8b25549957b01d6e13874e39c8fa574cea4
LordSA <96365910+LordSA@users.noreply.github.com>
2022-05-22 13:34:52 +0530

    Update __init__.py

--- DaisyXMusic/services/pytgcalls/__init__.py
+++ DaisyXMusic/services/pytgcalls/__init__.py
@@ -1,5 +1,5 @@
-from VC.services.queues import queues
+from DaisyXMusic.services.queues import queues
 
-from .pytgcalls import pytgcalls, run
+from DaisyXMusic.services.pytgcalls import pytgcalls, run
 
 __all__ = ["queues", "pytgcalls", "run"]
